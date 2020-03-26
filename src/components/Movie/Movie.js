0e37391136d0d2521a594360d26f9ee4d7bb8d17
eron-salling <eron.salling@generalassemb.ly>
2020-03-26 15:05:30 -0400

    Add react-crud bones of Movie.js file

---
+++ src/components/Movie/Movie.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import { Link, Redirect } from 'react-router-dom'
+
+// Import Axios:
+import axios from 'axios'
+// Import apiConfig:
+import apiUrl from '../apiConfig'
+
+class Movie extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      movie: null,
+      deleted: false
+    }
+  }
+
+  componentDidMount () {
+    axios(`${apiUrl}/movies/${this.props.match.params.id}`)
+      .then(res => {
+        this.setState({ movie: res.data.movie })
+      })
+      .catch(console.error)
+  }
+
+  delete = (event) => {
+    axios.delete(`${apiUrl}/movies/${this.props.match.params.id}`)
+      .then(() => {
+        this.setState({ deleted: true })
+      })
+      .catch(console.error)
+  }
+
+  render () {
+    // Destructure from state:
+    const { movie, deleted } = this.state
+    let movieJSX
+
+    // 3 states:
+    // If movie is `null`, we are loading
+    if (!movie) {
+      movieJSX = <img src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif"/>
+    } else if (deleted) {
+      movieJSX = <Redirect to="/movies"/>
+    } else {
+      // We have a movie, display it!
+      movieJSX = (
+        <div>
+          <h3>{movie.title}</h3>
+          <p>Director: {movie.director}</p>
+          <p>Year Released: {movie.year}</p>
+          <button onClick={this.delete}>Delete Movie</button>
+          <Link to={`/movies/${this.props.match.params.id}/edit`}>
+            <button>Update Movie</button>
+          </Link>
+        </div>
+      )
+    }
+
+    return (
+      movieJSX
+    )
+  }
+}
+
+export default Movie
