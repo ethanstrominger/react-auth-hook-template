2ee6ff18ae40e6a9ef2244b2c2de6c637ee504fd
eron-salling <eron.salling@generalassemb.ly>
2020-03-26 13:29:35 -0400

    Add annotations to App and AuthenticatedRoute

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react'
 import { Route } from 'react-router-dom'
 
+// Import all custom components for our App
 import AuthenticatedRoute from '../AuthenticatedRoute/AuthenticatedRoute'
 import AutoDismissAlert from '../AutoDismissAlert/AutoDismissAlert'
 import Header from '../Header/Header'
@@ -9,27 +10,50 @@ import SignIn from '../SignIn/SignIn'
 import SignOut from '../SignOut/SignOut'
 import ChangePassword from '../ChangePassword/ChangePassword'
 
+// We want to have state at the highest level possible in our app
+// So `App` is a class component
 class App extends Component {
   constructor () {
     super()
 
+    // Setup state: hold a reference to the user and all message alerts
     this.state = {
       user: null,
       msgAlerts: []
     }
   }
 
+  // This method is passed as a prop to `SignIn` and `SignUp`
+  // It is used to set the user on the state after successful sign in
+  // Sign in happens automatically after as successful sign up
+  // We declare this in `App` so it modifies the state of `App` not a child component
   setUser = user => this.setState({ user })
 
+  // This method is passed as a prop to `SignOut` or any component that needs to clear the user
+  // It sets the `user` to `null`
   clearUser = () => this.setState({ user: null })
 
+  // This method is passed as a prop to almost every component (any component that wants to display messages)
+  // Accepts a `heading`, `message`, and `variant`
+  // It sets the state of the `msgAlerts` array to add in the new message it receives
   msgAlert = ({ heading, message, variant }) => {
+    // The spread (...) operator allows us to create a copy of the current `msgAlerts` and add a new one to that array
+    // This then sets the state with the updated array
     this.setState({ msgAlerts: [...this.state.msgAlerts, { heading, message, variant }] })
   }
 
+  // This renders our app!
   render () {
+    // Destructure from the state
     const { msgAlerts, user } = this.state
 
+    // We have to return JSX!
+    // We pass the user to the `Header` to have it display the correct links
+    // Map over the `msgAlerts` array and display one `AutoDismissAlert` for each object
+    // For routing, we have `Route` and `AuthenticatedRoute`:
+    // Routes can be used for regular routing!
+    // AuthenticatedRoute can be used if we only want to allow access to a route after a user has signed in
+    // We MUST pass `user` as a prop to the `AuthenticatedRoute`s we use
     return (
       <Fragment>
         <Header user={user} />
@@ -42,7 +66,7 @@ class App extends Component {
           />
         ))}
         <main className="container">
-          <Route path='/sign-up' render={() => (
+          <Route path='/sign-up' render={props => (
             <SignUp msgAlert={this.msgAlert} setUser={this.setUser} />
           )} />
           <Route path='/sign-in' render={() => (
