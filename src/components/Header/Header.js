8aa9f3c21e865bfc2dcd605beea3aa4c81f9f43e
Ethan Strominger <ethanstrominger@Ethans-Air.fios-router.home>
2020-03-31 13:01:21 -0400

    Add View Movies link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,7 @@ const authenticatedOptions = (
   <Fragment>
     <Nav.Link href="#change-password">Change Password</Nav.Link>
     <Nav.Link href="#sign-out">Sign Out</Nav.Link>
+    <Nav.Link href="#movies">View Movies</Nav.Link>
     <Nav.Link href="#movie-create">Create A Movie</Nav.Link>
   </Fragment>
 )
